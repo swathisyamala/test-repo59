e069d97732ed9ee579fc48126380a3c86a3bfa82
swathisyamala <swathisyamalad@gmail.com>
2021-02-21 00:51:46 +0530

    modify by dev1

--- hello.java
+++ hello.java
@@ -1,8 +1,8 @@
-class hello()
+class hello()    # from dev1
 {
-	public static void main()
+	public static void main() # from dev1 
 	{
-		sysyem.out("bhavani")
+		sysyem.out("bhavani" # from dev1)
 	}}
 
 
